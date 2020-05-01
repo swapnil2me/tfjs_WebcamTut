c137b358340b2a9e78f3ff8c6589f4c173d9dd9c
swapnil2me <swapnilmore@iisc.ac.in>
2020-05-01 10:23:24 +0530

    Using webcam for continuous prediction

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const webcamElement = document.getElementById('webcam');
 let net;
 
 async function app() {
@@ -7,10 +8,24 @@ async function app() {
   net = await mobilenet.load();
   console.log('Successfully loaded model');
 
-  // Make a prediction through the model on our image.
-  const imgEl = document.getElementById('img');
-  const result = await net.classify(imgEl);
-  console.log(result);
+  // Create an object from Tensorflow.js data API which could capture image
+  // from the web camera as Tensor.
+  const webcam = await tf.data.webcam(webcamElement);
+  while (true) {
+    const img = await webcam.capture();
+    const result = await net.classify(img);
+
+    document.getElementById('console').innerText = `
+      prediction: ${result[0].className}\n
+      probability: ${result[0].probability}
+    `;
+    // Dispose the tensor to release the memory.
+    img.dispose();
+
+    // Give some breathing room by waiting for the next animation frame to
+    // fire.
+    await tf.nextFrame();
+  }
 }
 
 app();

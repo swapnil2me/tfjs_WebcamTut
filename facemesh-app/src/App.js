01c8ec3c29dd87a1183d65331284b31acd401602
Swapnil More <swapnilmore@iisc.ac.in>
2020-05-01 20:39:43 +0530

    Continuous Face tracking, but has a bug, dosent stop after starting

--- facemesh-app/src/App.js
+++ facemesh-app/src/App.js
@@ -1,17 +1,19 @@
 import * as tf from '@tensorflow/tfjs';
-import logo from './ModiandI.jpg';
-import React, {useRef} from 'react';
+// import logo from './ModiandI.jpg';
+import React, {useState, useRef} from 'react';
 import * as d3 from 'd3';
 import './App.css';
 
-var model;
+var model,webcamElement,webcam,img,predictions;
 var facemesh = require('@tensorflow-models/facemesh');
 
 async function loadFacenet() {
         model = await facemesh.load();
         console.log("Facenet Loaded");
+         webcamElement = document.getElementById('webcam');
+         webcam = await tf.data.webcam(webcamElement);
+         img = await webcam.capture();
       };
-loadFacenet();
 
 var line = d3.line()
          .x(function(d,i){return d.x;})
@@ -20,41 +22,39 @@ var line = d3.line()
 
 function App() {
 
+  const [timeseriesMode, setTimeseriesMode] = useState(false);
+
   let j = 0;
   const svgRef = useRef();
+
   async function main() {
 
     var svg = d3.select(svgRef.current);
+    svg.selectAll("*").remove();
     // Pass in a video stream (or an image, canvas, or 3D tensor) to obtain an
     // array of detected faces from the MediaPipe graph.
-    const webcamElement = document.getElementById('webcam');
-    const webcam = await tf.data.webcam(webcamElement);
 
-    while (true) {
+    img = await webcam.capture();
+    predictions = await model.estimateFaces(img);
+    // const predictions = await model.estimateFaces(document.getElementById('face'));
+    for (let i = 0; i < predictions.length; i++) {
 
-      const img = await webcam.capture();
-      const predictions = await model.estimateFaces(img);
-      // const predictions = await model.estimateFaces(document.getElementById('face'));
-      for (let i = 0; i < predictions.length; i++) {
-        svg.selectAll("*").remove();
-        let br = predictions[i].boundingBox.bottomRight[0];
-        let tl = predictions[i].boundingBox.topLeft [0];
-        var dataArray = [{x:tl[0],y:br[1]},
-                         {x:tl[0],y:tl[1]},
-                         {x:br[0],y:tl[1]},
-                         {x:br[0],y:br[1]},
-                         {x:tl[0],y:br[1]}];
-        svg.append("path")
-            .attr("fill","none")
-            .attr("stroke","yellow")
-            .attr("d",line(dataArray));
-        console.log(j++);
-      }
-      img.dispose();
-      await tf.nextFrame();
+    let br = predictions[i].boundingBox.bottomRight[0];
+    let tl = predictions[i].boundingBox.topLeft[0];
+    var dataArray = [{x:tl[0],y:br[1]},
+                   {x:tl[0],y:tl[1]},
+                   {x:br[0],y:tl[1]},
+                   {x:br[0],y:br[1]},
+                   {x:tl[0],y:br[1]}];
+    svg.append("path")
+      .attr("fill","none")
+      .attr("stroke","yellow")
+      .attr("d",line(dataArray));
+    console.log(j++);
     }
 
-
+    img.dispose();
+    await tf.nextFrame();
 
     // if (predictions.length > 0) {
     //   for (let i = 0; i < predictions.length; i++) {
@@ -73,7 +73,19 @@ function App() {
 
   return (
     <div className="App">
-      <button onClick={() => main()} >Find Face</button>
+      <button onClick={() => loadFacenet()} >Init</button>
+      <button onClick={() => main()} >Find Face Once</button>
+      <button onClick={(event)=>{
+                        setTimeseriesMode(!timeseriesMode);
+                        clearInterval(main);
+                        if (timeseriesMode) {
+                          setInterval(main, 1000);
+                          console.log("swap");
+                        }
+                      }}>
+      Continuous Mode: {timeseriesMode ? 'True' : 'False'}
+      </button>
+
       <header className="App-header">
         <div id="videoContainer" className="container">
           <video autoPlay playsInline muted id="webcam" width="224" height="224" className="centered"></video>

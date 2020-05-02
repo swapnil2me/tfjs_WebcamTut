0b887b783a0ce0609925ea7986100e357d72526f
Swapnil More <swapnilmore@iisc.ac.in>
2020-05-02 11:20:28 +0530

    Extracting facial features from annotations, instead of manual way

--- facemesh-app/src/App.js
+++ facemesh-app/src/App.js
@@ -6,9 +6,6 @@ import './App.css';
 
 var model,webcamElement,webcam,img,predictions;
 const offset = 20;
-var eyeRight = [33,246,161,160,159,158,157,173,133,153,154,153,145,144,163,7];
-var eyeLeft = [362,398,384,385,386,387,388,468,263,249,390,373,374,380,381,382];
-var mouth = [78,191,80,81,82,13,312,311,310,415,308,324,318,402,317,14,87,178,88,95];
 var facemesh = require('@tensorflow-models/facemesh');
 
 async function loadFacenet() {
@@ -18,11 +15,12 @@ async function loadFacenet() {
          webcam = await tf.data.webcam(webcamElement);
          img = await webcam.capture();
       };
+loadFacenet();
 
-var line = d3.line()
-         .x(function(d,i){return d.x;})
-         .y(function(d,i){return d.y;})
-         .curve(d3.curveStep);
+// var line = d3.line()
+//          .x(function(d,i){return d.x;})
+//          .y(function(d,i){return d.y;})
+//          .curve(d3.curveStep);
 
 function App() {
 
@@ -43,71 +41,39 @@ function App() {
     // const predictions = await model.estimateFaces(document.getElementById('face'));
     for (let i = 0; i < predictions.length; i++) {
 
-      const keypoints = predictions[i].scaledMesh;
+      let features = ['leftEyeLower0','leftEyeLower1','leftEyeLower2','leftEyeLower3',
+                      'leftEyeUpper0','leftEyeUpper1','leftEyeUpper2',
+                      'rightEyeLower0','rightEyeLower1','rightEyeLower2','rightEyeLower3',
+                      'rightEyeUpper0','rightEyeUpper1','rightEyeUpper2',
+                      'lipsLowerInner','lipsLowerOuter','lipsUpperInner','lipsUpperOuter'
+                    ]
 
-      var dataArray = keypoints.map((e,i) => {
-                                    return {x:e[0]-1.8*offset,y:e[1]-offset};
-        });
+      // console.log(predictions[i].annotations);
+      for (var k = 0; k < features.length; k++) {
+        console.log(predictions[i].annotations,features[k]);
+        const keypoints = predictions[i].annotations[features[k]];
 
-      var dataArray1 = keypoints.map((e,i) => {
-        if (eyeRight.includes(i)) {
-            return {x:e[0],y:e[1]};
+        if (keypoints.length>0) {
+          var dataArray = keypoints.map((e,l) => {
+                                        return {x:e[0]-1.8*0,y:e[1]-0};
+                                        });
+
+          svg.append("g").attr("class","fuel")
+            .selectAll("circle")
+            .data(dataArray)
+            .enter().append("circle")
+                    .attr("cx",function(d){return d.x;})
+                    .attr("cy",function(d){return d.y;})
+                    .attr("fill","yellow")
+                    .attr("r","1.0");
+
+          // svg.append("path")
+          //   .attr("fill","none")
+          //   .attr("stroke","yellow")
+          //   .attr("d",line(dataArray1));
         }
-        else {
-          return -10;
-        }
-      });
-      dataArray1 = dataArray1.filter(cord => cord !== -10);
 
-      var dataArray2 = keypoints.map((e,i) => {
-        if (eyeLeft.includes(i)) {
-            return {x:e[0],y:e[1]};
-        }
-        else {
-          return -10;
-        }
-      });
-      dataArray2 = dataArray2.filter(cord => cord !== -10);
-
-      var dataArray3 = keypoints.map((e,i) => {
-        if (mouth.includes(i)) {
-            return {x:e[0],y:e[1]};
-        }
-        else {
-          return -10;
-        }
-      });
-      dataArray3 = dataArray3.filter(cord => cord !== -10);
-
-      // let br = predictions[i].boundingBox.bottomRight[0];
-      // let tl = predictions[i].boundingBox.topLeft[0];
-      // var dataArray = [{x:tl[0],y:br[1]},
-      //                {x:tl[0],y:tl[1]},
-      //                {x:br[0],y:tl[1]},
-      //                {x:br[0],y:br[1]},
-      //                {x:tl[0],y:br[1]}];
-
-      svg.append("g").attr("class","fuel")
-        .selectAll("circle")
-        .data(dataArray)
-        .enter().append("circle")
-                .attr("cx",function(d){return d.x;})
-                .attr("cy",function(d){return d.y;})
-                .attr("fill","yellow")
-                .attr("r","1.0");
-
-      // svg.append("path")
-      //   .attr("fill","none")
-      //   .attr("stroke","yellow")
-      //   .attr("d",line(dataArray1));
-      // svg.append("path")
-      //   .attr("fill","none")
-      //   .attr("stroke","yellow")
-      //   .attr("d",line(dataArray2));
-      // svg.append("path")
-      //   .attr("fill","none")
-      //   .attr("stroke","yellow")
-      //   .attr("d",line(dataArray3));
+      }
       console.log(j++);
     }
 
